655f04edf1fc706002e52fe0f6ccea266a8cf7de
Drew Neil <andrew.jr.neil@gmail.com>
2010-09-30 10:54:17 +0100

    Created a stripped down interface with placeholders.
    The refresh button doesn't appear at the moment.

--- javascripts/geotweets.js
+++ javascripts/geotweets.js
@@ -4,35 +4,23 @@ Ext.setup({
 	icon: 'icon.png',
 	glossOnIcon: false,
 	onReady: function() {
-		var timeline = new Ext.Component({
-			title: 'Timeline',
-			cls: 'timeline',
-			scroll: 'vertical',
-			tpl: [
-				'<tpl for=".">',
-					'<div class="tweet">',
-							'<div class="avatar"><img src="{profile_image_url}" /></div>',
-							'<div class="tweet-content">',
-								'<h2>{from_user}</h2>',
-								'<p>{text}</p>',
-							'</div>',
-					'</div>',
-				'</tpl>'
-			]
-		});
 
-		var map = new Ext.Map({
-			title: 'Map',
-			getLocation: true,
-			mapOptions: {
-				zoom: 12
-			}
-		});
+		var timeline = {
+			title: "Timeline",
+			html: "A list of local twits",
+			cls: 'card1'
+		}
+
+		var map = {
+			title: "Map",
+			html: "Here be dragons",
+			cls: 'card2'
+		}
 
 		var panel = new Ext.TabPanel({
 			fullscreen: true,
 			animation: 'slide',
-			items: [map, timeline]
+			items: [timeline, map]
 		});
 
 		var tabBar = panel.getTabBar();
@@ -44,48 +32,8 @@ Ext.setup({
 			dock: 'right',
 			stretch: false,
 			align: 'center',
-			handler: refresh
+			handler: refresh	// leave this out until later?
 		});
-
-		var refresh = function() {
-			var coords = map.geo.coords;
-
-			Ext.util.JSONP.request({
-				url: 'http://search.twitter.com/search.json',
-				callbackKey: 'callback',
-				params: {
-					geocode: coords.latitude + ',' + coords.longitude + ',' + '5mi',
-					rpp: 30
-				},
-				callback: function(data) {
-					data = data.results;
-
-					// Update the tweets in timeline
-					timeline.update(data);
-
-					// Add points to the map
-					for (var i = 0, ln = data.length; i < ln; i++) {
-						var tweet = data[i];
-
-						// If the tweet is geo-tagged, use that to display marker
-						if (tweet.geo && tweet.geo.coordinates) {
-							var position = new google.maps.LatLng(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
-							addMarker(tweet, position);
-						}
-					}
-				}
-			});
-		};
-
-		// These are all Google Maps APIs
-		var addMarker = function(tweet, position) {
-			var marker = new google.maps.Marker({
-				map: map.map,
-				position: position
-			});
-		};
-
-		map.geo.on('update', refresh);
 
 	}
 });
