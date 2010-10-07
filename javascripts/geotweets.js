a7ef76900deb7569db43e6db7ee319d593f0365f
Drew Neil <andrew.jr.neil@gmail.com>
2010-10-07 14:22:07 +0100

    Change mockTwitterApi for simple list of Tweet objects.

--- javascripts/geotweets.js
+++ javascripts/geotweets.js
@@ -48,40 +48,15 @@ Ext.setup({
 		});
 
 		refresh = function() {
-			tweet_list = mockTwitterAPI.results;
+			var tweet = {
+				text: "Lorem ipsum dolor sit amet",
+				from_user: "nelstrom",
+				profile_image_url: "http://bit.ly/nelstrom-avatar"
+			};
+			var tweets = [tweet,tweet,tweet,tweet,tweet,tweet,tweet]
 			timeline.update(tweet_list);	// Update the tweets in timeline
 		};
 
 		refresh();
 	}
 });
-
-var mockTwitterAPI = {
-	"results": [
-		{
-			"profile_image_url": "http://a3.twimg.com/profile_images/740067459/my_square_head_640x480_normal.jpg",
-			"from_user": "paulanthonywils",
-			"text": "Had my Death Star for several weeks now. Verdict unchanged. Necessary as a tool of imperial oppression. No use to me as an everyday thing."
-		},
-		{
-			"profile_image_url": "http://a2.twimg.com/profile_images/1101388618/avatar1_normal.JPG",
-			"from_user": "aedison",
-			"text": "We've all been talking about your paranoia."
-		},
-		{
-			"profile_image_url": "http://a0.twimg.com/profile_images/598755044/faps-interro-twitavatar_normal.jpg",
-			"from_user": "FakeAPStylebook",
-			"text": "If you have a ladder first and then a farmer later, the ladder is the former and the farmer is the latter."
-		},
-		{
-			"profile_image_url": "http://a3.twimg.com/profile_images/1104534595/kellyoxford21-300x300_normal.png",
-			"from_user": "kellyoxford",
-			"text": "Enrolling your daughter in Gymnastics: 1% chance she'll be an athlete. 100% chance she'll be the drunk girl doing backflips in a bikini."
-		},
-		{
-			"profile_image_url": "http://a2.twimg.com/profile_images/421637246/Tim_normal.jpg",
-			"from_user": "timbray",
-			"text": "Variation: 'There are only two hard problems in Computer Science: cache invalidation, naming things, and off-by-one errors.'"
-		}
-	]
-}
