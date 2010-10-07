a04f0d4ad24c474ded713a9459703c392ba93936
Drew Neil <andrew.jr.neil@gmail.com>
2010-10-07 14:21:40 +0100

    Tweaked whitespace formatting of template.

--- javascripts/geotweets.js
+++ javascripts/geotweets.js
@@ -12,13 +12,13 @@ Ext.setup({
 			scroll: 'vertical',
 			tpl: [
 				'<tpl for=".">',
-					'<div class="tweet">',
-							'<div class="avatar"><img src="{profile_image_url}" /></div>',
-							'<div class="tweet-content">',
-								'<h2>{from_user}</h2>',
-								'<p>{text}</p>',
-							'</div>',
-					'</div>',
+				'	<div class="tweet">',
+				'			<div class="avatar"><img src="{profile_image_url}" /></div>',
+				'			<div class="tweet-content">',
+				'				<h2>{from_user}</h2>',
+				'				<p>{text}</p>',
+				'			</div>',
+				'	</div>',
 				'</tpl>'
 			]
 		});
