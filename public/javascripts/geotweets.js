03ea706b62b3427d1f2fbed6d8211d346972abad
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-24 01:19:17 +0000

    Revert from 01 branch to skeleton app.

--- public/javascripts/geotweets.js
+++ public/javascripts/geotweets.js
@@ -2,41 +2,5 @@ Ext.setup({
 	tabletStartupScreen: 'tablet_startup.png',
 	phoneStartupScreen: 'phone_startup.png',
 	icon: 'icon.png',
-	glossOnIcon: false,
-	onReady: function() {
-		var timeline, map, panel, tabBar;
-
-		timeline = {
-			title: "Timeline",
-			html: "A list of local twits",
-			cls: 'card1'
-		}
-
-		map = {
-			title: "Map",
-			html: "Here be dragons",
-			cls: 'card2'
-		}
-
-		panel = new Ext.TabPanel({
-			fullscreen: true,
-			cardSwitchAnimation: 'slide',
-			ui: 'light',
-			items: [timeline, map]
-		});
-
-		panel.getTabBar().add([
-			{xtype: 'spacer'},
-			{
-				xtype: 'button',
-				iconMask: true,
-				iconCls: 'refresh',
-				ui: 'plain',
-				style: 'margin:0;'
-			}
-		]);
-		panel.getTabBar().doLayout();
-
-	}
+	glossOnIcon: false
 });
-
