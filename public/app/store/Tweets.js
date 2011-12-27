30104bbc3aa76eb4f1fe2adcba1a709e17e5f490
Drew Neil <andrew.jr.neil@gmail.com>
2011-12-27 17:26:52 +0000

    Set up a JsonP proxy to query the Twitter API.

--- public/app/store/Tweets.js
+++ public/app/store/Tweets.js
@@ -1,11 +1,17 @@
 Ext.define('GeoTweets.store.Tweets', {
     extend: 'Ext.data.Store',
     model: 'GeoTweets.model.Tweet',
-    data: [
-        {
-            text: "Lorem ipsum dolor sit amet",
-            from_user: "nelstrom",
-            profile_image_url: "http://bit.ly/nelstrom-avatar"
+
+    autoLoad: true,
+    proxy: {
+        type: 'jsonp',
+        url: 'http://search.twitter.com/search.json',
+        extraParams: {
+            rpp: 30,
+            q: 'bored'
         },
-    ]
+        reader: {
+            root: 'results'
+        }
+    }
 });
