39c075e264dd7ceec6e452858cf1e5eac026c6dd
Drew Neil <andrew.jr.neil@gmail.com>
2011-12-27 19:24:18 +0000

    Add a 'geo' field to the Tweet model.

--- public/app/model/Tweet.js
+++ public/app/model/Tweet.js
@@ -1,4 +1,9 @@
 Ext.define('GeoTweets.model.Tweet', {
     extend: 'Ext.data.Model',
-    fields: ['from_user', 'profile_image_url', 'text']
+    fields: [
+        'from_user',
+        'profile_image_url',
+        'text',
+        'geo'
+    ]
 })
