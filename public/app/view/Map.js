5213efce4b1afe00baa172130fa4da8bb2fdb4b7
Drew Neil <andrew.jr.neil@gmail.com>
2011-12-28 17:16:00 +0000

    Log something on GeoLocation locationupdate event.

--- public/app/view/Map.js
+++ public/app/view/Map.js
@@ -8,7 +8,11 @@ Ext.define('GeoTweets.view.Map', {
 
     constructor: function() {
         this.callParent(arguments);
+
         this._geo.setAutoUpdate(false);
+        this._geo.on('locationupdate', function() {
+            console.log('updating location')
+        })
     }
 
 })
