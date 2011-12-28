fef7557981d21baf5ae295486a6c65f3869dc96a
Drew Neil <andrew.jr.neil@gmail.com>
2011-12-28 16:42:34 +0000

    Draw a marker for each geo-tagged tweet.

--- public/app/controller/Main.js
+++ public/app/controller/Main.js
@@ -19,13 +19,15 @@ Ext.define('GeoTweets.controller.Main', {
             'button[action=refresh]': {
                 tap: 'fetchTweets'
             }
-        })
+        });
+        window.tweetBubble = new google.maps.InfoWindow();
+        window.mapPanel = this.getLocalmap();
     },
 
     fetchTweets: function() {
         var store = this.getTimeline().getStore(),
             coords = this.getLocalmap()._geo.config,
-            geocode = coords.latitude + ',' + coords.longitude + ',' + '5mi';
+            geocode = coords.latitude + ',' + coords.longitude + ',' + '100mi';
 
         store.getProxy().setExtraParam('geocode', geocode);
         store.load({
@@ -38,14 +40,36 @@ Ext.define('GeoTweets.controller.Main', {
 
     updateMap: function(tweetlist) {
         console.log('updating map...')
+        
 
         for (var i = 0, ln = tweetlist.length; i < ln; i++) {
-            var tweet = tweetlist[i];
-            console.log(tweet.data.geo);
+            var tweet = tweetlist[i].data;
+// console.log(tweet.data.geo);
             if (tweet.geo && tweet.geo.coordinates) {
-                addMarker(tweet);
+                this.addMarker(tweet);
+            } else {
+                console.log('no geo data')
             }
         }
         
+    },
+    
+    addMarker: function(tweet) {
+        console.log(tweet.geo);
+        var latLng = new google.maps.LatLng(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
+        console.log(latLng);
+        var mapPanel = this.getLocalmap();
+        window.mapPanel = mapPanel;
+        
+        var marker = new google.maps.Marker({
+            map: mapPanel.getMap(),
+            position: latLng
+        });
+
+        google.maps.event.addListener(marker, "click", function() {
+            console.log('clicked a marker')
+            window.tweetBubble.setContent(tweet.text);
+            window.tweetBubble.open(mapPanel.map, marker);
+        });
     }
 });
